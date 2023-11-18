ea675cab3fd199cefb270bd842a4694a7c4e0dbb
Vadim515 <vadimis307@gmail.com>
2023-11-18 23:12:03 +0500

    Добавил папочку с конфигом

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,23 @@
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import {
+    IBuildOptions,
+    IBuildPaths,
+} from './types/config';
+
+export function buildPlugins({ paths }: IBuildOptions): webpack.WebpackPluginInstance[] {
+
+    let progressPlugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
+    ];
+
+    return progressPlugins;
+};
---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,13 @@
+import { ResolveOptions } from 'webpack';
+
+export function buildResolvers (): ResolveOptions {
+
+    return {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js',
+        ],
+    }
+
+}
