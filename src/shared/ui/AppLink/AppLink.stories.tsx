34b3bfd41a57c9ba0e815ca9d721d24bb5004ba1
Vadim515 <vadimis307@gmail.com>
2023-12-03 22:45:14 +0500

    test app link

--- src/shared/ui/AppLink/AppLink.stories.tsx
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -3,7 +3,7 @@ import type {
     StoryObj
 } from '@storybook/react'
 import { Theme } from '../../../app/providers/ThemeProvider'
-import { themeDecorator } from '../../../shared/config/storybook/decorators/themeDecorator'
+import { themeDecorator } from '../../config/storybook/decorators/themeDecorator'
 import { AppLink } from './AppLink'
 
 export default { component: AppLink }
@@ -21,17 +21,45 @@ const meta = {
     }
 
 } satisfies Meta<typeof AppLink>
+// const Template: ComponentStory<typeof AppLink> = (args) => <AppLink {...args} />
+// export const Primary = Template.bind({})
+// Primary.args = {
+//     children: 'Text',
+//     theme: AppLinkTheme.PRIMARY
+// }
+//
+// export const Secondary = Template.bind({})
+// Secondary.args = {
+//     children: 'Text',
+//     theme: AppLinkTheme.SECONDARY
+// }
+//
+// export const PrimaryDark = Template.bind({})
+// PrimaryDark.args = {
+//     children: 'Text',
+//     theme: AppLinkTheme.PRIMARY
+// }
+// PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)]
+//
+// export const SecondaryDark = Template.bind({})
+// SecondaryDark.args = {
+//     children: 'Text',
+//     theme: AppLinkTheme.SECONDARY
+// }
+// SecondaryDark.decorators = [ThemeDecorator(Theme.DARK)]
 
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
     args: {
+        pattern: 'primary',
         children: 'Текст ссылки'
     }
 }
 
 export const PrimaryLight: Story = {
     args: {
+        pattern: 'primary',
         children: 'Текст ссылки'
     }
 }
@@ -39,12 +67,14 @@ PrimaryLight.decorators = [themeDecorator(Theme.Light)]
 
 export const Button: Story = {
     args: {
+        pattern: 'button',
         children: 'Текст ссылки'
     }
 }
 
 export const ButtonLight: Story = {
     args: {
+        pattern: 'button',
         children: 'Текст ссылки'
     }
 }
