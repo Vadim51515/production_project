8be038b351afbb119d510cd67d958d4406031adb
Vadim515 <vadimis307@gmail.com>
2023-12-03 21:39:06 +0500

    change screenshot

--- src/shared/config/storybook/decorators/styleDecorator.tsx
+++ src/shared/config/storybook/decorators/styleDecorator.tsx
@@ -1,4 +1,5 @@
 import 'app/styles/index.scss'
 import { type Story } from '@storybook/react'
-import React from 'react'
-export const styleDecorator = (StoryComponent: Story) => <StoryComponent />
+
+// TODO Разобраться, что нужно возвращать
+export const styleDecorator = (story: () => Story) => story()
