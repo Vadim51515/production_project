d633f453c6792ea907d5f22e741811cd5051c6b8
Vadim515 <vadimis307@gmail.com>
2023-12-03 23:44:21 +0500

    test app link

--- src/shared/ui/AppLink/AppLink.stories.tsx
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -5,7 +5,7 @@ import type {
 import { Theme } from '../../../app/providers/ThemeProvider'
 import { themeDecorator } from '../../config/storybook/decorators/themeDecorator'
 import { AppLink } from './AppLink'
-
+import React from 'react'
 export default { component: AppLink }
 const meta = {
     title: 'shared/AppLink',
@@ -21,39 +21,15 @@ const meta = {
     }
 
 } satisfies Meta<typeof AppLink>
-// const Template: ComponentStory<typeof AppLink> = (args) => <AppLink {...args} />
-// export const Primary = Template.bind({})
-// Primary.args = {
-//     children: 'Text',
-//     theme: AppLinkTheme.PRIMARY
-// }
-//
-// export const Secondary = Template.bind({})
-// Secondary.args = {
-//     children: 'Text',
-//     theme: AppLinkTheme.SECONDARY
-// }
-//
-// export const PrimaryDark = Template.bind({})
-// PrimaryDark.args = {
-//     children: 'Text',
-//     theme: AppLinkTheme.PRIMARY
-// }
-// PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)]
-//
-// export const SecondaryDark = Template.bind({})
-// SecondaryDark.args = {
-//     children: 'Text',
-//     theme: AppLinkTheme.SECONDARY
-// }
-// SecondaryDark.decorators = [ThemeDecorator(Theme.DARK)]
 
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-    args: {
-        pattern: 'primary',
-        children: 'Текст ссылки'
+    render: () => {
+        return (
+            <AppLink pattern={'primary'} to='/'>
+                {'Текст ссылки'}
+            </AppLink>)
     }
 }
 
