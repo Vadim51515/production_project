0954440de2218d77546fc86909bd7e16f8b17069
Vadim515 <vadimis307@gmail.com>
2023-12-04 20:43:40 +0500

    fix text

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 
 const MainPage = () => {
-    const { t } = useTranslation('main')
+    // const { t } = useTranslation('main')
 
     return (
         <div>
-            <h1>{t('Главная страница')}</h1>
+            {/* <h1>{t('Главная страница')}</h1> */}
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <h1>Главная страница</h1>
             {/* <h1>{t('Главная тестовая страница1')}</h1> */}
             {/* /!* eslint-disable-next-line i18next/no-literal-string *!/ */}
             {/* <Button pattern='outline'>Outline btn</Button> */}
