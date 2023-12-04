8eddb8c6803a637c28ed7e837eda7f1356ee21e3
Vadim515 <vadimis307@gmail.com>
2023-12-04 20:39:00 +0500

    fix text

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button } from '../../../shared/ui/Button'
 
 const MainPage = () => {
     const { t } = useTranslation('main')
@@ -8,11 +7,11 @@ const MainPage = () => {
     return (
         <div>
             <h1>{t('Главная страница')}</h1>
-            <h1>{t('Главная тестовая страница1')}</h1>
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <Button pattern='outline'>Outline btn</Button>
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <Button pattern='clear'>Clear btn</Button>
+            {/* <h1>{t('Главная тестовая страница1')}</h1> */}
+            {/* /!* eslint-disable-next-line i18next/no-literal-string *!/ */}
+            {/* <Button pattern='outline'>Outline btn</Button> */}
+            {/* /!* eslint-disable-next-line i18next/no-literal-string *!/ */}
+            {/* <Button pattern='clear'>Clear btn</Button> */}
         </div>
     )
 }
