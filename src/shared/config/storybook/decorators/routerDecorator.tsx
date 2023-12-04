3ef2e4358ca931ee10ab2b24f80c5e5717b8dbf4
Vadim515 <vadimis307@gmail.com>
2023-12-04 20:26:07 +0500

    - i18 text

--- src/shared/config/storybook/decorators/routerDecorator.tsx
+++ src/shared/config/storybook/decorators/routerDecorator.tsx
@@ -1,11 +1,15 @@
 import 'app/styles/index.scss'
 import { type Story } from '@storybook/react'
 import React from 'react'
+import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
+import i18nForTests from '../../i18n/i18nForTests'
 export const routerDecorator = (StoryComponent: Story) => {
     return (
         <BrowserRouter>
-            <StoryComponent />
+            <I18nextProvider i18n={i18nForTests}>
+                <StoryComponent />
+            </I18nextProvider>
         </BrowserRouter>
     )
 }
