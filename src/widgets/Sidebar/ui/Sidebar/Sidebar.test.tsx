41e689b129f900f65565bea6cdf7321d3ed222b8
Vadim515 <vadimis307@gmail.com>
2023-12-06 23:16:11 +0500

    28 Сайдбар. Состояния кнопки. UI Screenshot test report

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -3,26 +3,17 @@ import {
     screen
 } from '@testing-library/react'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar'
-import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { componentRender } from '../../../../shared/lib/tests/componentRender'
 
 describe('Sidebar', () => {
     test('Sidebar рендериться', () => {
-        renderWithTranslation(
-            <BrowserRouter>
-                <Sidebar />
-            </BrowserRouter>
-        )
+        componentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('Кнопка смены размера Sidebar работает', () => {
-        renderWithTranslation(
-            <BrowserRouter>
-                <Sidebar />
-            </BrowserRouter>
-        )
+        componentRender(<Sidebar />)
         const toggleSizeBtn = screen.getByTestId('sidebar-toggle-size')
         expect(toggleSizeBtn).toBeInTheDocument()
         fireEvent.click(toggleSizeBtn)
