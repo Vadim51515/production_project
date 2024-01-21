035bda8ae72fe467face82264db608c142df75e3
Vadim515 <vadimis307@gmail.com>
2024-01-21 22:28:37 +0500

    71 HTML report для тестов

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -73,7 +73,18 @@ const config: Config = {
         __IS_DEV__: true,
         __API__: '',
         __PROJECT__: 'jest'
-    }
+    },
+
+    // Use this configuration option to add custom reporters to Jest
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true
+        }]
+    ]
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
@@ -135,9 +146,6 @@ const config: Config = {
 
     // Run tests from one or more projects
     // projects: undefined,
-
-    // Use this configuration option to add custom reporters to Jest
-    // reporters: undefined,
 
     // Automatically reset mock state before every test
     // resetMocks: false,
